2dfe829614a0a37169422464ea15e73fc180deea
Axel Somerseth Cordova <axelsomerseth@gmail.com>
2021-12-09 22:59:44 +0000

    second test added

--- index.test.js
+++ index.test.js
@@ -3,3 +3,7 @@ const getAboutUsLink = require("./index");
 test("Returns about-us for english language", () => {
     expect(getAboutUsLink("en-US")).toBe("/about-us");
 });
+
+test("Returns about-us for portuguese language", () => {
+    expect(getAboutUsLink("pt-BR")).toBe("/sobre-nos");
+});git add .
